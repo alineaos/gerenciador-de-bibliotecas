9f586cd2d637d604c44b3bfd03b72ac02e221280
Aline <alineoliveiradesantana2013@gmail.com>
2026-04-22 15:05:56 -0300

    :tada: Commit inicial

---
+++ src/main/java/io/github/alineaos/librarymanager/LibraryManagerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.alineaos.librarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagerApiApplication.class, args);
+	}
+
+}
