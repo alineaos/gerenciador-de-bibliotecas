53feeeac584aa91be3ff6d27e0037e63ad850e49
Aline <alineoliveiradesantana2013@gmail.com>
2026-04-28 18:41:13 -0300

    :recycle: refactor(service): Ajustar exceção no CustomUserDetailsService
    - Alterar NotFoundException para UsernameNotFoundException
    - Atualizar a mensagem que da exceção

--- src/main/java/io/github/alineaos/librarymanager/security/service/CustomUserDetailsService.java
+++ src/main/java/io/github/alineaos/librarymanager/security/service/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package io.github.alineaos.librarymanager.security.service;
 
-import io.github.alineaos.librarymanager.exception.NotFoundException;
 import io.github.alineaos.librarymanager.repository.UserRepository;
 import io.github.alineaos.librarymanager.security.domain.UserAuthenticated;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +12,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 private final UserRepository userRepository;
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
                 .map(UserAuthenticated::new)
-                .orElseThrow(() -> new NotFoundException("User not found."));
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid credentials"));
     }
 }
