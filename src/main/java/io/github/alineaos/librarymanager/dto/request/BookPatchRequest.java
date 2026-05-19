3e097a08a7545bf1b15226ecb1eb582db456014c
Aline <alineoliveiradesantana2013@gmail.com>
2026-05-18 21:18:12 -0300

    :bug: fix(book): Adicionar Constraints de validação no BookPatchRequest
    - Adicionar Constraints para auxiliar na validação do BookPatchRequest

--- src/main/java/io/github/alineaos/librarymanager/dto/request/BookPatchRequest.java
+++ src/main/java/io/github/alineaos/librarymanager/dto/request/BookPatchRequest.java
@@ -1,12 +1,24 @@
 package io.github.alineaos.librarymanager.dto.request;
 
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
 import java.time.Year;
 
 public record BookPatchRequest (
         String title,
+
         String author,
+
         String publisher,
+
+        @PastOrPresent(message = "The year can not be in the future.")
         Year year,
+
+        @Positive(message = "The edition must be greater than or equal to 1.")
         Integer edition,
+
+        @Pattern(regexp = "^(\\d{10}|\\d{13})$", message = "ISBN must be 10 or 13 digits.")
         String isbn
 ){}
